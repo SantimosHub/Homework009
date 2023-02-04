cf37de7c4ee26cdf846a4b50efcf5aa61e8648bc
«Santimos» <stosoleg@gmail.com>
2023-02-04 14:32:11 +0300

    Done

---
+++ Task064/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+string AllNaturalNumbers(int inNumber)
+{
+    if (inNumber == 0) return String.Empty;
+    return $"{inNumber} " + AllNaturalNumbers(inNumber - 1);
+}
+
+int number = new Random().Next(0, 20);
+Console.WriteLine($"{number} -> {AllNaturalNumbers(number)} ");
---
+++ Task066/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int AllNumbersSum(int inFirstNumber, int inSecondNumber)
+{
+    if (inFirstNumber == inSecondNumber) return inSecondNumber;
+    return inFirstNumber + AllNumbersSum(inFirstNumber + 1, inSecondNumber);
+}
+
+int firstNumber = new Random().Next(0, 20);
+int secondNumber = new Random().Next(0, 20);
+if (firstNumber > secondNumber)
+{
+    int temp = firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = temp;
+}
+
+Console.WriteLine($"Сумма чисел между {firstNumber} и {secondNumber} = {AllNumbersSum(firstNumber, secondNumber)} ");
